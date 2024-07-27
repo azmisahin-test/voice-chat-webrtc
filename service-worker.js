652fc9687c86b86c4771a5e8e182e2ebbf842bd5
Azmi SAHIN <azmisahin@outlook.com>
2024-07-27 22:52:04 +0300

    servıcec workser

--- service-worker.js
+++ service-worker.js
@@ -3,21 +3,37 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(clients.claim()); // Uygulamanın hemen yeni service worker'ı kullanmasını sağlar.
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== "cache-v1") {
+              // Eski önbellekleri temizleyin
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        return clients.claim(); // Uygulamanın hemen yeni service worker'ı kullanmasını sağlar.
+      })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open("cache-v1").then((cache) => {
-      return cache.match(event.request).then((response) => {
-        return (
-          response ||
-          fetch(event.request).then((fetchResponse) => {
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          return caches.open("cache-v1").then((cache) => {
             cache.put(event.request, fetchResponse.clone());
             return fetchResponse;
-          })
-        );
-      });
+          });
+        })
+      );
     })
   );
 });
